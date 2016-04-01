f9b7f6c88ec53dea826ab57d093455a26557239d
ChriD <chris_d85@hotmail.com>
2016-04-01 22:02:22 +0200

    1.0.0

---
+++ source/Raumserver/getActualLibs.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cp ../../../RaumkernelLib/source/libs/linux_x86/raumkernel.so libs/linux_x86/raumkernel.so
+cp ../../../RaumserverLib/source/libs/linux_x86/raumserver.so libs/linux_x86/raumserver.so
+
+cp ../../../RaumkernelLib/source/libs/linux_ARMV6/raumkernel.so libs/linux_ARMV6/raumkernel.so
+cp ../../../RaumserverLib/source/libs/linux_ARMV6/raumserver.so libs/linux_ARMV6/raumserver.so
+
+cp ../../../RaumkernelLib/source/libs/linux_ARMV7/raumkernel.so libs/linux_ARMV7/raumkernel.so
+cp ../../../RaumserverLib/source/libs/linux_ARMV7/raumserver.so libs/linux_ARMV7/raumserver.so
+
