55fb93f4f2269623a5e5512f5c8602a82f6fbbf1
Christian Dürnberger <chris_d85@hotmail.com>
2016-04-04 20:43:11 +0200

    1.0.0

---
+++ source/RaumserverInstaller/includes/raumserverInstallerView.h
@@ -0,0 +1,52 @@
+//
+// The MIT License (MIT)
+//
+// Copyright (c) 2016 by ChriD
+//
+// Permission is hereby granted, free of charge,  to any person obtaining a copy of
+// this software and  associated documentation  files  (the "Software"), to deal in
+// the  Software  without  restriction,  including without limitation the rights to
+// use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+// the Software,  and to permit persons to whom the Software is furnished to do so,
+// subject to the following conditions:
+//
+// The above copyright notice and this  permission notice  shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE  SOFTWARE  IS  PROVIDED  "AS IS",  WITHOUT  WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+// FOR A PARTICULAR PURPOSE  AND NONINFRINGEMENT.  IN NO EVENT SHALL THE AUTHORS OR
+// COPYRIGHT HOLDERS BE  LIABLE FOR ANY CLAIM,  DAMAGES OR OTHER LIABILITY, WHETHER
+// IN  AN  ACTION  OF  CONTRACT,  TORT  OR  OTHERWISE,  ARISING  FROM, OUT OF OR IN
+// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+#pragma once
+#ifndef RAUMKSERVERINSTALLER_H
+#define RAUMKSERVERINSTALLER_H
+
+#include "sciter-x-window.hpp"
+#include "json/json.h"
+
+static RECT wrc = { 100, 100, 600, 400 };
+
+class frame : /*public sciter::window*/
+    public sciter::window
+    , public sciter::event_handler
+{
+public:
+    frame() : window(SW_MAIN | SW_ALPHA | SW_POPUP | SW_ENABLE_DEBUG, wrc) {}       
+
+    json::value getNetworkAdapterInformation();
+
+    sciter::value testCpp(json::value param1, json::value param2);
+
+    BEGIN_FUNCTION_MAP
+        FUNCTION_0("getNetworkAdapterInformation", getNetworkAdapterInformation);
+        FUNCTION_2("testCpp", testCpp);        
+    END_FUNCTION_MAP
+
+};
+
+
+#endif
---
+++ source/RaumserverInstaller/raumserverInstallerView.cpp
@@ -0,0 +1,61 @@
+
+#include "raumserverInstallerView.h"
+
+
+//http://sciter.com/developers/embedding-principles/
+//http://www.terrainformatica.com/2012/11/sciter-ui-application-architecture/
+
+json::value frame::getNetworkAdapterInformation()
+{
+    //json::value returnValue;    
+
+    Json::Value root, networkAdapter;
+    //std::string my_encoding = root.get("my-encoding", "UTF-32").asString();
+
+
+   // const Json::Value my_plugins = root["my-plug-ins"];
+    //for (int index = 0; index < my_plugins.size(); ++index)  // Iterates over the sequence elements.
+    //    yourlib::loadPlugIn(my_plugins[index].asString());
+
+ //   yourlib::setIndentLength(root["my-indent"].get("length", 3).asInt());
+ //   yourlib::setIndentUseSpace(root["my-indent"].get("use_space", true).asBool());
+
+    // ...
+    // At application shutdown to make the new configuration document:
+    // Since Json::Value has implicit constructor for all value types, it is not
+    // necessary to explicitly construct the Json::Value object:
+    //root["networkAdapterInformations"
+
+    networkAdapter["networkAdapter"]["name"] = "Adapter 1";
+    networkAdapter["networkAdapter"]["SubnetMask"] = "255.0.0.0";
+    root["networkAdapterInformations"].append(networkAdapter);
+
+    networkAdapter["networkAdapter"]["name"] = "Adapter 2";
+    networkAdapter["networkAdapter"]["SubnetMask"] = "255.0.0.0";
+    root["networkAdapterInformations"].append(networkAdapter);
+
+    
+    //returnValue.set_item("Name", sciter::value("Test1"));
+    //returnValue.set_item("SubnetMask:", sciter::value("255.0.0.0"));
+
+    auto test = root.toStyledString();
+
+    return sciter::value(root.toStyledString());
+}
+
+
+sciter::value frame::testCpp(json::value param1, json::value param2)
+{
+    //window* pw = ...    
+    this->call_function("NetworkAdapterSelection.addNetworkAdapter", "Test1");
+    this->call_function("NetworkAdapterSelection.addNetworkAdapter", "Test2");    
+
+    /*
+    WINDOWPLACEMENT wp = {};
+    wp.length = sizeof (WINDOWPLACEMENT);
+    //GetWindowPlacement(Sess.M_hdl, &wp);
+    const sciter::value arr[4] = { wp.rcNormalPosition.left, wp.rcNormalPosition.top, wp.rcNormalPosition.right - wp.rcNormalPosition.left, wp.rcNormalPosition.bottom - wp.rcNormalPosition.top };
+    return sciter::value(arr);
+    */
+    return sciter::value(1);
+}
