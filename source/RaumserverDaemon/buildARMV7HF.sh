a37318ac89c169500f1376d8708c11e8d8614ec6
ChriD <chris_d85@hotmail.com>
2016-04-27 18:49:26 +0200

    1.0.0

--- source/RaumserverDaemon/buildARMV7HF.sh
+++ source/RaumserverDaemon/buildARMV7HF.sh
@@ -4,4 +4,6 @@ make arch=ARMV7HF
 /bin/cp -rf libs/linux_ARMV7/raumserver.so bin/linux_ARMV7/libs/linux_ARMV7/raumserver.so
 /bin/cp -rf libs/linux_ARMV7/raumkernel.so bin/linux_ARMV7/libs/linux_ARMV7/raumkernel.so
 /bin/cp -rf raumsrvDaemon bin/linux_ARMV7/raumsrvDaemon
+/bin/cp -rf raumserver bin/linux_ARMV7/raumserver
+/bin/cp -rf raumserver.xml bin/linux_ARMV7/raumserver.xml
 make arch=ARMV7HF clean
--- source/RaumserverDaemon/buildX86.sh
+++ source/RaumserverDaemon/buildX86.sh
@@ -4,4 +4,6 @@ make arch=X86
 /bin/cp -rf libs/linux_x86/raumserver.so bin/linux_x86/libs/linux_x86/raumserver.so
 /bin/cp -rf libs/linux_x86/raumkernel.so bin/linux_x86/libs/linux_x86/raumkernel.so
 /bin/cp -rf raumsrvDaemon bin/linux_x86/raumsrvDaemon
+/bin/cp -rf raumserver bin/linux_x86/raumserver
+/bin/cp -rf raumserver.xml bin/linux_x86/raumserver.xml
 make arch=X86 clean
