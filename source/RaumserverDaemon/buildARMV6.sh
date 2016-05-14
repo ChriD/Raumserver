7874fb2f04b50c8e9f544ce650d154a81e698137
ChriD <chris_d85@hotmail.com>
2016-05-14 16:20:35 +0200

    1.0.0

---
+++ source/RaumserverDaemon/buildARMV6.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+./getActualLibs.sh
+make arch=ARMV6 clean
+make arch=ARMV6
+/bin/cp -rf libs/linux_ARMV6/raumserver.so bin/linux_ARMV6/libs/linux_ARMV6/raumserver.so
+/bin/cp -rf libs/linux_ARMV6/raumkernel.so bin/linux_ARMV6/libs/linux_ARMV6/raumkernel.so
+/bin/cp -rf raumsrvDaemon bin/linux_ARMV6/raumsrvDaemon
+/bin/cp -rf raumserver.xml bin/linux_ARMV6/raumserver.xml
+make arch=ARMV6 clean
