711a0c78c3c818930439d5964738bcf2ffb26b56
Christian Dürnberger <chris_d85@hotmail.com>
2016-06-09 18:22:04 +0200

    1.0.0

--- source/RaumserverInstaller/binaries/updateBinaries.bat
+++
@@ -1,3 +0,0 @@
-xcopy ..\..\RaumserverDaemon\bin\linux_ARMV5  ARMV5\raumserverDaemon /SY
-xcopy ..\..\RaumserverDaemon\bin\linux_ARMV7  ARMV7\raumserverDaemon /SY
-xcopy ..\..\RaumserverDaemon\bin\linux_AMDGEODE  AMDGEODE\raumserverDaemon /SY
