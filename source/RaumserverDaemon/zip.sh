f8d6468fa838b71a22584ac47a2cc37c5dffca3c
ChriD <chris_d85@hotmail.com>
2016-11-11 17:23:35 +0100

    1.0.2

--- source/RaumserverDaemon/zip.sh
+++ source/RaumserverDaemon/zip.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 ARCH=$1
+rm -rf bin/linux_$ARCH/
 mkdir -p bin
 mkdir -p bin/linux_$ARCH
+mkdir -p bin/linux_$ARCH/logs
 
 #copy build stuff
 rsync -aP build/linux_$ARCH/static/* bin/linux_$ARCH
+/bin/cp -rf S99raumserver bin/linux_$ARCH/S99raumserver
+
+#clear old logs
+rm -rf bin/linux_$ARCH/logs/*
+touch bin/linux_$ARCH/logs/_dummy.txt
 
 #zip it
 cd bin
